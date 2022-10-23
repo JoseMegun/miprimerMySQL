c77ddff35e5f6d75a4fbcce4890e3a3083db4b3f
Jose Megun <jose.cama@vallegrande.edu.pe>
2022-10-22 22:10:42 -0500

    He creado una base de datos con una tabla

--- dbMinimarket.sql
+++ dbMinimarket.sql
@@ -1,2 +1,19 @@
 /*Listar base de datos del servidor:*/
 SHOW DATABASES;
+
+/*Crear base de datos */
+CREATE DATABASE dbMinimarket;
+
+/*Poner en uso la base de datos*/
+USE dbMinimarket;
+
+/*Crear una tabla */
+CREATE TABLE VENDEDOR (
+    CODVEND CHAR(6),
+    NOMVEND VARCHAR(50),
+    APEVEND VARCHAR(50),
+    DOCVEND VARCHAR(10),
+    CELVEND CHAR(9),
+    DOMVEND VARCHAR(80),
+    PRIMARY KEY (CODVEND)
+);
